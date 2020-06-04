292e03659b467035b3a6def166e52beef6e320ef
BuildTools <unconfigured@null.spigotmc.org>
2020-06-04 15:24:48 +0200

    test

--- encode.js
+++ encode.js
@@ -1,13 +1,23 @@
-var old = html = document.getElementById("editor").value;
-document.getElementById("url-result").value = window.location.hostname + "?p=" + btoa(old);
+var old = document.getElementById("editor").value;
+old = btoa(old);
+old = old.split("+").join("~");
+old = old.split("=").join("|");
+old = old.split("/").join("_");
+document.getElementById("url-result").value = "https://" + window.location.hostname + "?p=" + old;
+new QRCode(document.getElementById("qrcode"), ("https://" + window.location.hostname + "?p=" + old));
 
 function update_iframe(){
     var html = document.getElementById("editor").value;
+    var tmp = html;
+    html = btoa(html);
+    html = html.split("+").join("~");
+    html = html.split("=").join("|");
+    html = html.split("/").join("_");
     var framed = document.getElementById("out")
     var shurl = document.getElementById("url-result");
     if (old != html){
-        framed.src = "data:text/html;base64," + btoa(html);
-        shurl.value = window.location.hostname + "?p=" + btoa(html);
+        framed.src = "data:text/html;base64," + btoa(tmp);
+        shurl.value = "https://" + window.location.hostname + "?p=" + html;
     }
     old = html
 }
@@ -19,4 +29,26 @@ function copyText() {
     alert("Copied to the clipboard");
 }
 
-setInterval(update_iframe, 1000);
+
+var urlParams = new URLSearchParams(window.location.search);
+var PageData = urlParams.get('p');
+
+if (PageData == null){
+    setInterval(update_iframe, 500);
+} else {
+    document.body.innerHTML = '';
+    document.body.style.backgroundImage = "none";
+    document.body.style.backgroundColor = "white";
+    var newFrame = document.createElement("iframe");
+    newFrame.setAttribute('style', 'width:100%');
+    newFrame.setAttribute('frameborder', '0');
+    newFrame.setAttribute('allowfullscreen', '');
+    newFrame.setAttribute('id', 'page');
+    document.body.appendChild(newFrame);
+    var x = document.getElementById('page'); 
+    x.style.height = "100%";
+    PageData = PageData.split("~").join("+");
+    PageData = PageData.split("|").join("=");
+    PageData = PageData.split("_").join("/");
+    x.src = "data:text/html;base64," + PageData;
+}
